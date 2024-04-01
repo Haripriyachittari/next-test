358569439d0a965d1b07067d1b8053827a982958
Haripriyachittari <hpvarma09@gmail.com>
2024-04-01 16:03:56 +0530

    chapter 11 completed

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
@@ -22,6 +23,7 @@ export async function fetchRevenue() {
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
+    noStore();
 
     // console.log('Data fetch completed after 3 seconds.');
 
@@ -40,6 +42,7 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+    noStore();
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
@@ -63,6 +66,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
+    noStore();
 
     const data = await Promise.all([
       invoiceCountPromise,
